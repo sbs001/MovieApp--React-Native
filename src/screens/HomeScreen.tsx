fa72e5b7c2e708b2a584035e3441f4cb572244e0
Sebastian <tassone_af@yahoo.com>
2021-08-09 19:22:51 -0300

    Feat: carousel on Home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { Dimensions } from "react-native";
 import { ActivityIndicator, Text, View } from "react-native";
+import Carousel from 'react-native-snap-carousel';
+
 import MovieCard from "../components/MovieCard/MovieCard";
 import { useMovies } from "../hooks/useMovies";
 
+
+const windowWidth = Dimensions.get('window').width
+
 export default function HomeScreen() {
 
-  const { moviesNowPlaying, loading} = useMovies();
+  const { moviesNowPlaying, loading } = useMovies();
 
-  if (loading) return <View><ActivityIndicator color='grey' size={70}/></View>
+  if (loading) return <View><ActivityIndicator color='grey' size={70} /></View>
 
   return (
     <View>
-      <MovieCard movie={moviesNowPlaying[0]} />
+      <View style={{ height: 440 }}>
+        <Carousel
+          data={moviesNowPlaying}
+          renderItem={({ item }: any) => <MovieCard movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   )
 }
