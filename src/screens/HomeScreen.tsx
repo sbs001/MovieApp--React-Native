9bfe565ad0418fa11671b76cdabee50cd09469d8
Sebastian <tassone_af@yahoo.com>
2021-08-09 15:47:55 -0300

    Add: now playing response interface

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 import { Text, View } from "react-native";
 import movieApi from "../api/MovieApi";
+import { NowPlaying } from "../interfaces/nowPlaying";
 
 export default function HomeScreen(){
 
   useEffect(()=>{
-    movieApi.get('/now_playing')
+    movieApi.get<NowPlaying>('/now_playing')
     .then(res => console.log(res.data))
   },[]);
 
